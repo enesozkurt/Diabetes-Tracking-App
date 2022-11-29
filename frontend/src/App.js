ba876b005cf4ecbb42dffbf40ae3cd3511f9a5c7
Enes Özkurt <enes.ozkurt4@gmail.com>
2022-11-29 13:40:50 -0800

    Adding Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {
     Route,
     Navigate,
 } from "react-router-dom";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import AuthPage from "./pages/Auth";
 import RecordsPage from "./pages/Records";
@@ -12,11 +13,14 @@ import RecordsPage from "./pages/Records";
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Navigate replace to="/auth" />} />
-            <Route path="/auth" element={<AuthPage/>} />
-            <Route path="/records" element={<RecordsPage />} />
-        </Routes>
+        <MainNavigation />
+        <main className="main-content">
+            <Routes>
+                <Route path="/" element={<Navigate replace to="/auth" />} />
+                <Route path="/auth" element={<AuthPage/>} />
+                <Route path="/records" element={<RecordsPage />} />
+            </Routes>
+        </main>
     </BrowserRouter>
   );
 }
